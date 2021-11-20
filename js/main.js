a8e2301dd827e74223640585d0b69ea782ff4af8
Danish <shaikhsaalim327@gmail.com>
2021-11-21 01:04:53 +0530

    final commit with cursor bugs

---
+++ js/main.js
@@ -0,0 +1,53 @@
+const allNum = document.querySelector("#number-grid");
+const calc = Array.from(allNum.children);
+let input = document.querySelector(".num-input");
+let result = document.querySelector(".result-input");
+var newInp = "";
+
+calc.forEach(e => {
+				e.addEventListener("click", () => {
+								
+								try {
+												if (e.classList[1] == "clear"){
+																input.value = "";
+																result.value = "";
+												}	
+												else if (e.classList[1] == "back"){
+																
+																index = input.value.charAt(input.selectionStart - 1);
+																input.value = input.value.replace(index, "");
+																input.focus()
+																
+												}																							
+												else if (e.classList[1] == "equal"){
+																if (input.value.search("÷") != "-1") { 
+																				newInp = input.value.replace("÷", "/");
+																				result.value = eval(newInp).toFixed(2);
+																} 
+																else if (input.value.search("×") != "-1") {
+																				newInp = input.value.replace("×", "*");
+																				result.value = eval(newInp);
+																} 
+																else if (input.value.search("%") != "-1") {
+																
+																				let percent = input.value.split("%");
+																				result.value = percent[0] / 100 * percent[1];
+																}															
+																else {																			
+																				result.value = eval(input.value);
+																}												
+																result.style.color = "green";
+				    				}
+				    				else {
+																input.value += e.innerHTML;
+												}
+				    			
+				    }	catch (e)	{
+												if (e.name == "SyntaxError" ) {
+															result.value = "expression error";
+															result.style.color = "red";
+												}
+								}													 							 																																								
+				});
+});
+
